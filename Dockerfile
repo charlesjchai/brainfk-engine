426eff8c740d46bb4c62672857de4a7c5f2ddb3e
Charles Chai <charlesxiaoanchai@gmail.com>
2026-09-07 22:28:24 -0400

    feat: Start transpiler

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM alpine:latest
-
-WORKDIR /app
-
-RUN apk add --no-cache build-base cmake
-
-COPY . .
-
-RUN cmake -B build -S . && cmake --build build
--- include/utils.hpp
+++ include/utils.hpp
@@ -1,16 +1,44 @@
 #pragma once
+#include <memory>
 #include <string>
+#include <unordered_set>
+#include <variant>
 
 namespace BrainFK {
-    enum class Mode {
-        Interpret,
-        Transpile,
-        Compile,
-    };
-    enum class Flag {
-        Optimize,
-    };
-
-    // Input can ONLY contain Brainf**k's 8 symbols (><+-.,[]), throws error if otherwise
-    void interpret(std::string_view, const std::unordered_set<Flag>&);
+enum class ASTToken;
 }
+using ASTValue = std::variant<BrainFK::ASTToken, int>;
+
+namespace BrainFK {
+enum class Mode {
+    Interpret,
+    Transpile,
+    Compile,
+};
+enum class Flag {
+    Optimize,
+};
+
+// AST Node to transpile into C
+enum class ASTToken {
+    // Add and Sub have one child, which is the int in the std::variant
+    Add,
+    Sub,
+    Left,
+    Right,
+    Print,
+    Input,
+    Loop,
+};
+struct ASTNode {
+    ASTValue value;
+    std::unique_ptr<ASTNode> child;
+};
+
+// Input can ONLY contain Brainf**k's 8 symbols (><+-.,[]), throws error if
+// otherwise
+void interpret(std::string_view program,
+               const std::unordered_set<Flag> &options);
+std::string transpile(std::string_view program,
+                      const std::unordered_set<Flag> &options);
+} // namespace BrainFK
